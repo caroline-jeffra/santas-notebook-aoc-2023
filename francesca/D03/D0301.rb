a32ad055e5597b83399fe19723733f2aa4fb1511
Francesca Santoriello <f.santoriello@hotmail.com>
2023-12-03 18:05:44 +0100

    D0300-r

--- francesca/D03/D0301.rb
+++ francesca/D03/D0301.rb
@@ -1,42 +1,35 @@
-require 'daru'
-
 class GearRatio
-    SYMBOLS = %w(* # + $)
-
+    require 'daru'
+    SYM_XP = /[*|#|+|$]/
+    
     attr_reader :frame, :sym_positions
     def initialize(txt_path)
         @frame         = parse_to_frame("#{__dir__}/#{txt_path}")
         @sym_positions = find_sym_positions()
     end
-
+    
     private
-
-    # Daru::DataFrame(10x10)
+    
+    # => Daru::DataFrame(10x10)
     def parse_to_frame(path)
         scheme = File.open(path).readlines.map { |line| line.chomp.split("") }        
         order  = (0...scheme.size).to_a.map { |i| "col_#{i}" }
 
         Daru::DataFrame.rows(scheme, order: order)
     end
-
-    # {"*" => [{:row=>1, :col=>"col_3"}, {}, {}], "+"=>[{:row=>5, :col=>"col_5"}],...}
+    
+    # => [{:row=>1, :col=>4}, {:row=>3, :col=>7},...]
     def find_sym_positions
-        positions = {}
-    
-        SYMBOLS.each do |symbol|
-          positions[symbol] = []
-          @frame.each_row.with_index do |row, row_i|
-            row.each_with_index do |value, col_i|
-              positions[symbol] << { row: row_i, col: col_i } if value == symbol
+        @frame.each_row.with_index.flat_map do |row, row_i|
+            row.each_with_index.filter_map do |value, col_i|
+                { row: row_i, col: col_i } if value.match?(SYM_XP)
             end
-          end
         end
-    
-        positions
-      end
+    end
 end
 
 ## TEST AREA ##
 decoder = GearRatio.new("./input.txt")
 
+pp decoder.frame
 pp decoder.sym_positions
