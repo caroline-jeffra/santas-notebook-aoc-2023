21c1fe4aa642f5a408b198ab9aefbd851d17121f
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-12-03 23:39:33 +0100

    3-12 part 1 pass

--- caroline/3-dec/3-dec.php
+++ caroline/3-dec/3-dec.php
@@ -1,44 +1,85 @@
 <?php
-$pattern = '/[^0-9.]/';
+$char_pattern = '/[^0-9.\n]/';
 $lines = file("input.txt");
 $y_count = 0;
 $char_positions = array();
+$nums = array();
 foreach( $lines as $line )
  {
     $x_count = 0;
     $chars = str_split($line);
+    $last_char_int = false;
+    $current_int_tracker = 0;
+    preg_match_all('/[0-9]+/', $line, $numbers);
     foreach( $chars as $char ){
-      if ( preg_match($pattern, $char) ){
+      if ( (preg_match($char_pattern, $char)) && $x_count !== 140 ){
         $position = new Position($x_count, $y_count);
         array_push($char_positions, $position);
-        print_r($position);
+      } elseif ( preg_match('/[0-9]/', $char) && ( $last_char_int === false )){
+        $new_int = new Part_Number($x_count, $y_count, (int)($numbers[0][$current_int_tracker]));
+        array_push($nums, $new_int);
+        $current_int_tracker += 1;
+      }
+      if ( preg_match('/[0-9]/', $char) ){
+        $last_char_int = true;
+      } else {
+        $last_char_int = false;
       }
       $x_count += 1;
-      echo 'x: ' . $x_count . '<br>';
     }
     $y_count += 1;
-    echo 'y: ' . $y_count . '<br>';
  }
-//  print_r($char_positions);
+
+$total_value = 0;
+foreach ($nums as $num){
+  $num_valid = false;
+  foreach ($char_positions as $char){
+    if ($char->y == $num->start->y || $char->y == $num->start->y - 1 || $char->y == $num->start->y + 1 ){
+      if (proximity_pass($num, $char)){
+        $num_valid = true;
+      }
+    }
+  }
+  if ($num_valid){
+    $total_value += $num->value;
+  }
+}
+echo '<br>' . $total_value;
 
  class Position {
   public $x = 0;
   public $y = 0;
 
-  function __constructor($x, $y){
+  function __construct($x, $y){
     $this->x = $x;
     $this->y = $y;
   }
-
-  function proximity_pass(Position $num_start_position, int $num){
-    $x = $this->x;
-    $y = $this->y;
-
-    // test x for proximity
-    if ( ($y === $num_start_position->y) && ( $x === ($num_start_position->x - 1) || $x === ($num_start_position->x + count_chars((string)$num) + 1) )){
-      return true;
-    }
-
-    // test y for proximity
-  }
  }
+
+class Part_Number {
+  public Position $start;
+  public int $length;
+  public int $value;
+
+  function __construct( int $x_val, int $y_val, int $int_value ){
+    $this->start = new Position($x_val, $y_val);
+    $this->length = strlen((string)$int_value);
+    $this->value = $int_value;
+  }
+}
+
+function proximity_pass(Part_Number $part, Position $special_character){
+  $char_x = $special_character->x;
+  $char_y = $special_character->y;
+
+  $num_x = $part->start->x;
+  $num_y = $part->start->y;
+  $num_length = $part->length;
+
+  if ( ($char_y == $num_y) && ( ($char_x == ($num_x - 1) )|| ($char_x == ($num_x + $num_length)))){
+    return true;
+  } else if ( (($char_y == $num_y + 1) || ($char_y == $num_y - 1) ) && (( $char_x >= ($num_x - 1) && ($char_x <= ($num_x + $num_length)) ))){
+    return true;
+  }
+  else return false;
+}
