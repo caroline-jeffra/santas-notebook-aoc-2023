964f67c5fa9c319e51fe132a3d3554a04211582a
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-04 12:42:15 +0100

    discarded recursive solution in favor of using an object/hash/dict/map

--- francesco/04/04.mjs
+++ francesco/04/04.mjs
@@ -5,33 +5,30 @@ const data = await fetch("https://adventofcode.com/2023/day/4/input", headers)
   .then(resp => resp.text())
   .then(text => text.split("\n").slice(0,-1))
 
+var sum = 0;
+const copies = {};
 
-const computeSums = (data, start, end, part) => {
-  var sum = 0;
-  var count = 0;
+data.forEach((card, i) => {
+  if(!(i in copies)) copies[i] = 0;
+  copies[i] += 1;
 
-  const copies = data.slice(start, end);
-  copies.forEach((card, i) => {
-    const [winning, scratched] = card.split(": ")[1].split(" | ").map(half => {
-      return half.split(/\s+/g).filter(Boolean);
-    })
-
-    const hits = scratched.filter(num => winning.includes(num));
-    const numHits = hits.length;
-    if(part == 1) {
-      const points = Number.parseInt(2 ** (numHits - 1));
-      sum += points
-    } else {
-      const nextRow = start + i + 1;
-      count += 1 + computeSums(data, nextRow, nextRow + numHits, part);
-    }
+  const [winning, scratched] = card.split(": ")[1].split(" | ").map(half => {
+    return half.split(/\s+/g).filter(Boolean);
   })
 
-  if (part == 1) return sum
-  else return count;
-}
+  const hits = scratched.filter(num => winning.includes(num));
+  const numHits = hits.length;
+  const points = Number.parseInt(2 ** (numHits - 1));
 
-const partOne = computeSums(data, 0, data.length, 1);
-const partTwo = computeSums(data, 0, data.length, 2);
+  for(let j = 0; j < numHits; j++) {
+    if(!(i + 1 + j in copies)) copies[i + 1 + j] = 0;
+    copies[i + 1 + j] += copies[i];
+  }
+  sum += points;
+})
+
+const partOne = sum;
+const partTwo = Object.values(copies).reduce((sum, v) => sum + v, 0);
+
 console.log(partOne);
 console.log(partTwo);
