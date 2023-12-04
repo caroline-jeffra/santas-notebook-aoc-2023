5f4297d8d65ba2838c0f1736331758e6448e315d
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-12-04 18:54:40 +0100

    4-12 part 2 pass

--- caroline/04-dec/main.php
+++ caroline/04-dec/main.php
@@ -2,6 +2,8 @@
 $cards = array();
 $matches = array();
 $total_score = 0;
+$card_qty = 0;
+$cards_to_count = array();
 $lines = file( "input.txt" );
 
 foreach ( $lines as $line ) {
@@ -10,15 +12,18 @@ foreach ( $lines as $line ) {
   $winners = format_numbers( $values[1] );
   $contents = format_numbers( $values[2] );
   $card = new Card( $id, $winners, $contents );
-  array_push( $cards, $card );
+  $card_qty += 1;
+  $cards[$card->id] = $card;
+  array_push( $cards_to_count, $card );
 }
+
+echo 'initial card quantity: ' . $card_qty . '<br>';
 
 foreach ( $cards as $card ){
   $matches = $card->match_finder();
   $total_score += $card->card_tally( $matches );
 }
-echo $total_score;
-
+echo 'total score of cards (part 1): ' . $total_score;
 
 function format_numbers( String $value_string ){
   $value_string = trim($value_string);
@@ -33,19 +38,49 @@ function format_numbers( String $value_string ){
   }
 }
 
+do {
+  $card_object = array_shift( $cards_to_count ); // this line will make your browser cry, apparently
+  $list = $card_object->new_cards;
+  foreach ( $list as $item ){
+    $current_card = $cards[$item];
+    array_push( $cards_to_count, $current_card );
+    $card_qty += 1;
+  }
+} while ( count( $cards_to_count ) > 0 );
+echo 'card quantity after going nuts: ' . $card_qty . '<br>';
+
 class Card {
   public int $id;
   public array $winners;
   public array $card_contents;
+  public int $matches;
+  public array $new_cards;
 
   function __construct( int $id, array $winners, array $contents ){
     $this->id = $id;
     $this->winners = $winners;
     $this->card_contents = $contents;
+    $this->match_tally();
+    $this->get_new_cards();
   }
 
   function match_finder(){
     return array_intersect( $this->winners, $this->card_contents );
+  }
+
+  function get_new_cards(){
+    $new_cards_array = array();
+    for ( $i = 1; $i < $this->matches; $i += 1 ){
+      if ( $this->id + $i < 204 ){
+        $card_num = $this->id + $i;
+        array_push( $new_cards_array, $card_num);
+      }
+    }
+    $this->new_cards = $new_cards_array;
+  }
+
+  function match_tally() {
+    $this->matches = count( $this->match_finder() );
   }
 
   function card_tally ( array $matches ){
