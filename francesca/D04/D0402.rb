4b63b622e27bc70282a520913bb98ac41075790c
Francesca Santoriello <f.santoriello@hotmail.com>
2023-12-05 13:06:53 +0100

    D0402(slowlogic)

--- francesca/D04/D0402.rb
+++ francesca/D04/D0402.rb
@@ -1,5 +1,3 @@
-require 'pry-byebug'
-
 def process_game_part(game_part)
     game_part.split(/\s/).map(&:to_i).reject(&:zero?)
 end
@@ -14,36 +12,26 @@ def parse_cards(filepath)
 end
 
 def compute_wins_per_card(cards)
-    cards.map.with_index { |card, idx| { idx+1 => (card[:winning] & card[:owned]).size } }
+    cards.map.with_index { |card, idx| { idx+1 => [(card[:winning] & card[:owned]).size] } }
 end
 
-# wins = [{1=>4}, {2=>2}, {3=>2}, {4=>1}, {5=>0}, {6=>0}]
 def compute_total_cards(wins)
-    begin
-        wins.each do |card|
-            # win = {1=>4}
+    wins.each do |card|
+        key   = card.keys.first
+        value = card[key].flatten.sum
 
-            next_upper = card.keys.first
-            next_lower = card[next_upper]
-            new_cards  = wins[next_upper..next_lower]
-            # new_cards = {2=>2}, {3=>2}, {4=>1}, {5=>0}
-            
-            new_cards.each do |new_card|
-                # new_card = {2=>2}
-                id  = new_card.keys.first
-                idx = id - 1
-                wins[idx][id] = [wins[idx][id]] + [new_card[id]]
-            end
+        new_cards  = wins[key..value]
+        new_cards.each do |new_card|
+            id  = new_card.keys.first
+            idx = id - 1
+            wins[idx][id] = ([wins[idx][id]] + [new_card[id]]).flatten
         end
-        # win = {2=>[2,2]}
-    rescue ArgumentError => e
-        return wins
-    end
+    end.flat_map { |hash| hash.values }.flatten.sum
 end
 
 ## TEST AREA ##
-cards = parse_cards("input.txt")
-wins  = compute_wins_per_card(cards)
+cards     = parse_cards("input.txt")
+wins      = compute_wins_per_card(cards)
+tot_cards = compute_total_cards(wins)
 
-p compute_total_cards(wins)
-# => [{1=>4}, {2=>[2, 2]}, {3=>[2, 2]}, {4=>[1, 1]}, {5=>[0, 0]}, {6=>0}]
+pp tot_cards
