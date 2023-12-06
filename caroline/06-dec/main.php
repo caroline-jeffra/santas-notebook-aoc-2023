14d5a8c73dbefbe2b2499311cc0322497c7e1289
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-12-06 09:11:31 +0100

    6-12 part 2 pass

--- caroline/06-dec/main.php
+++ caroline/06-dec/main.php
@@ -4,19 +4,13 @@ $record_values = array();
 foreach( $lines as $line ){
   $data = preg_split( '/([\s:]+)/', $line );
   $key = lcfirst( array_shift( $data ));
-  $record_values[$key] = $data;
+  $record_values[$key] = implode( $data );
 }
-print_r( $record_values );
-echo '<br>';
 
 $times = $record_values['time'];
 $distances = $record_values['distance'];
 
-$error_margin = 1;
-for ( $i = 0; $i < count($times) - 1; $i++ ){
-  $error_margin *= win_tester( (int)$times[$i], (int)$distances[$i] );
-}
-echo $error_margin;
+echo win_tester( (int)$times, (int)$distances );
 
 function win_tester ( int $time, int $record ){
   $win_states = 0;
