d9ac2c87f51afaa5bd3f7fe67e5e83a7bfd2af77
Daniele Trapani <daniele.trapani96@gmail.com>
2023-12-06 10:24:51 +0100

    day6 part2

--- daniele/day6/main.go
+++ daniele/day6/main.go
@@ -21,42 +21,33 @@ func main() {
 	}
 
 	timesStrings := strings.Split(lines[0], " ")
-	times := removeEmptyAndConvert(timesStrings)
+	time := removeEmptyAndConvert(timesStrings)
 
 	distancesStrings := strings.Split(lines[1], " ")
-	distances := removeEmptyAndConvert(distancesStrings)
+	distance := removeEmptyAndConvert(distancesStrings)
 
-	race_results := []int{}
+	race_results := 0
 
-	for time_index, time := range times {
-		race_result := 0
-		goal := distances[time_index]
-		for i := 1; i < time; i++ {
-			dps := i
-			if (time-i)*dps > goal {
-				race_result++
-			}
-
+	for i := 1; i < time; i++ {
+		dps := i
+		if (time-i)*dps > distance {
+			race_results++
 		}
-		race_results = append(race_results, race_result)
+
 	}
 
-	output := 1
-	for _, result := range race_results {
-		output = output * result
-	}
-
-	println(output)
+	println(race_results)
 }
 
-func removeEmptyAndConvert(arr []string) []int {
-	var r []int
+func removeEmptyAndConvert(arr []string) int {
+	var r []string
 	for _, el := range arr {
 		if el != "" {
-			elNum, _ := strconv.Atoi(el)
-			r = append(r, elNum)
+			r = append(r, el)
 		}
 	}
+	numString := strings.Join(r, "")
+	num, _ := strconv.Atoi(numString)
 
-	return r
+	return num
 }
