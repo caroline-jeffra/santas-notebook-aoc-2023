cee5f4110fd84cc8640bb71c0fe11645bd693b5c
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-06 10:56:26 +0100

    Used transpose

--- francesco/06/06.rb
+++ francesco/06/06.rb
@@ -11,6 +11,7 @@ def num_possible_wins((time, record))
   zeroes = [0.5 * (time + dscr), 0.5 * (time - dscr)]
   min_time = zeroes.min.floor + 1
   max_time = largest_int_lt(zeroes.max)
+
   max_time - min_time + 1
 end
 
@@ -26,9 +27,9 @@ p1_data = data.map do |l|
    .map(&:to_i)
 end
 
-p1_pairs = p1_data.first.each_with_index.map { |_, i| [p1_data.first[i], p1_data.last[i]] }
+p1_pairs = p1_data.transpose
 
-part_one = p1_pairs.reduce(1) { |m, pair| m * num_possible_wins(pair) }
+part_one = p1_pairs.reduce(1) { |margin, pair| margin * num_possible_wins(pair) }
 puts part_one
 
 ################################################################################
