0f139bac56fdf6a010d38a887c8003602d6272ed
Francesca Santoriello <f.santoriello@hotmail.com>
2023-12-11 16:10:21 +0100

    D1101(galaxyid)

--- francesca/D11/D1101.rb
+++ francesca/D11/D1101.rb
@@ -8,21 +8,22 @@ def parse_galaxies(filepath)
 end
 
 class Matrix
-    def is_empty?(char) = char.match?(/\./)
-    def all_empty?(arr) = arr.all? { |char| is_empty?(char) }
+    def is_galaxy?(char) = char.match?(/\#/)
+    def is_empty?(char)  = char.match?(/\./)
+    def all_empty?(arr)  = arr.all? { |char| is_empty?(char) }
+        
+    def empty_rows = (0...row_count).select { |idx| all_empty?(row(idx).to_a) }
+    def empty_cols = (0...column_count).select { |idx| all_empty?(column(idx).to_a) }
 
-    def empty_rows = (0...row_count).select { |idx| self.all_empty?(row(idx).to_a) }
-    def empty_cols = (0...column_count).select { |idx| self.all_empty?(column(idx).to_a) }
-    
     def insert_row_at_index(idx, new_row)
-        rows = to_a
+        rows = self.to_a
         rows.insert(idx, new_row)
         Matrix[*rows]
     end
 
     def insert_column_at_index(idx, new_col)
         cols    = column_vectors.map(&:to_a)
-        new_col = new_col.to_a.slice(0, row_count) + Array.new(row_count - new_col.size, ".")
+        new_col = new_col.to_a.slice(0, self.row_count) + Array.new(self.row_count - new_col.size, ".")
 
         cols.insert(idx, new_col)
         Matrix.columns(cols)
@@ -34,24 +35,34 @@ class Matrix
 
         empty_rows.each_with_index do |idx, i|
             new_row    = Vector[*Array.new(column_count, ".")]
-            new_matrix = new_matrix.insert_row_at_index(idx, new_row)
+            new_matrix = new_matrix.insert_row_at_index(idx+1, new_row)
 
             empty_rows[i+1] += 1 unless empty_rows[i+1].nil?
         end
 
         empty_cols.each_with_index do |idx, i|
             new_col = Vector[*Array.new(row_count, ".")]
-            new_matrix = new_matrix.insert_column_at_index(idx, new_col)
+            new_matrix = new_matrix.insert_column_at_index(idx+1, new_col)
 
             empty_cols[i+1] += 1 unless empty_cols[i+1].nil?
         end
         return new_matrix
     end
+
+    def assign_galaxy_id
+        id    = 0
+        @rows = self.to_a.map do |row|
+          row.map do |char|
+            id += 1 if is_galaxy?(char)
+            char.match?(/\#/) ? id : char 
+          end
+        end
+    end
 end
 
 ## TEST AREA ##
 galaxies_matrix = parse_galaxies("input.txt")
-pp galaxies_matrix
+                .expand_cosmos
+                .assign_galaxy_id
 
-galaxies_matrix = galaxies_matrix.expand_cosmos
-pp galaxies_matrix.column(9)
+pp galaxies_matrix
